1c76b75d3a234edb7f50850efeda97f064d2cfce
Qadir Hussain <qadir.hussain99@gmail.com>
2023-06-18 21:32:12 +0100

    Updating Jumping on the clouds solution

--- interview-preparation-kit/src/dev/techieguy/warmup/JumpingOnTheClouds.java
+++ interview-preparation-kit/src/dev/techieguy/warmup/JumpingOnTheClouds.java
@@ -6,14 +6,18 @@ import java.util.List;
 
 public class JumpingOnTheClouds {
     public static void main(String[] args) {
-        System.out.println(solution(Arrays.asList(0, 0, 1, 0, 0, 1, 0)));
+        System.out.println(solution(Arrays.asList(0, 0)));
     }
     static int solution(final List<Integer> list) {
         int count = 0;
-        for (int index = 0; index < list.size() - 1; index++) {
-            if(index + 2 < list.size() && list.get(index + 2) == 0)
-                index++;
-            count++;
+        int size = list.size();
+        if (size >= 2 && size <= 100 && (list.contains(0) || list.contains(1)) && list.get(0) == 0 && list.get(size - 1) == 0) {
+            System.out.println("Inside if condition");
+            for (int index = 0; index < size - 1; index++) {
+                if(index + 2 < size && list.get(index + 2) == 0)
+                    index++;
+                count++;
+            }
         }
         return count;
     }
