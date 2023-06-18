194e7e2ce918e64ecfdd22651e2290c6d4c7ced5
Qadir Hussain <qadir.hussain99@gmail.com>
2023-06-18 21:16:45 +0100

    Adding Jumping on the clouds solution

--- interview-preparation-kit/src/dev/techieguy/warmup/JumpingOnTheClouds.java
+++ interview-preparation-kit/src/dev/techieguy/warmup/JumpingOnTheClouds.java
@@ -1,2 +1,21 @@
-package dev.techieguy.warmup;public class JumpingOnTheClouds {
+package dev.techieguy.warmup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JumpingOnTheClouds {
+    public static void main(String[] args) {
+        System.out.println(solution(Arrays.asList(0, 0, 1, 0, 0, 1, 0)));
+    }
+    static int solution(final List<Integer> list) {
+        int count = 0;
+        for (int index = 0; indsex < list.size() - 1; index++) {
+            if(index + 2 < list.size() && list.get(index + 2) == 0) {
+                index++;
+            }
+            count++;
+        }
+        return count;
+    }
 }
