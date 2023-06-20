ce689e4efdc9d177dd21320dcaee56bcb30442e9
Qadir Hussain <qadir.hussain99@gmail.com>
2023-06-21 00:47:53 +0100

    Updated 2D hour glass array problem solution

--- interview-preparation-kit/src/dev/techieguy/array/Array2D.java
+++ interview-preparation-kit/src/dev/techieguy/array/Array2D.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 public class Array2D {
 
     private static final int NUMBER_OF_LINES = 6;
-    private static int array[][]   = new int[NUMBER_OF_LINES][NUMBER_OF_LINES];
-    private static Scanner input   = new Scanner(System.in);
-    private static int result      = 0;
+    private static int array[][] = new int[NUMBER_OF_LINES][NUMBER_OF_LINES];
+    private static Scanner input = new Scanner(System.in);
+    private static int result = 0;
     private static String line;
     private static String arrayLine[];
     private static int temp;
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < NUMBER_OF_LINES; i++) {
+            line = input.nextLine();
+            arrayLine = line.split(" ");
+            for (int j = 0; j < NUMBER_OF_LINES; j++)
+                array[i][j] = Integer.valueOf(arrayLine[j]);
+        }
+
+        for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 temp = calculateHourGlass(array, i, j);
+                if (i == 0 && j == 0)
+                    result = temp;
+                result = Math.max(temp, result);
             }
         }
+        System.out.println(result);
     }
 
     private static int calculateHourGlass(int[][] array, int i, int j) {
-
+        return array[i][j] + array[i][j + 1] + array[i][j + 2] + array[i + 1][j + 1] + array[i + 2][j] + array[i + 2][j + 1] + array[i + 2][j + 2];
     }
 }
