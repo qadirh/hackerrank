f1437101352f79169c182c79d6f97497593276ac
Qadir Hussain <qadir.hussain99@gmail.com>
2023-06-23 23:48:53 +0100

    Updated array left rotation solution

--- interview-preparation-kit/src/dev/techieguy/array/ArrayLeftRotation.java
+++ interview-preparation-kit/src/dev/techieguy/array/ArrayLeftRotation.java
@@ -1,8 +1,10 @@
 package dev.techieguy.array;
 
+import java.util.Scanner;
+
 public class ArrayLeftRotation {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
     }
 }
